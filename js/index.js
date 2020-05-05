938ca6f034bfed2e5212313a7314e93c7c0df333
UB <yyb2535@gmail.com>
2020-05-05 23:27:58 +0900

    push

--- js/index.js
+++ js/index.js
@@ -147,3 +147,39 @@ for (let i = 0; i < 5; i++) {
   txtresult += '\n';
 }
 console.log('16번 문제\n' + txtresult);
+
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++)
+  {
+    if(array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 7)); // -1
+
+function binarySearch (array, target) {
+  let start = 0;
+  let end = array.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) * 0.5);
+    if (array[mid] === target) return mid;
+    else if (array[mid] > target) end = mid - 1;
+    else start = mid + 1;
+  }
+  return -1
+}
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
